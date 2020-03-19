030444ff7c63d0dd26153e4b82da21daf4e8bb07
adrianbparra <adrianplane12@gmail.com>
2020-03-18 19:55:22 -0700

    added the motd in .env to display in server url

--- api/server.js
+++ api/server.js
@@ -7,6 +7,12 @@ const server = express();
 
 server.use(helmet());
 
+server.get("/", (req,res) => {
+    const MOTD = process.env.MOTD || "Welcome to Adrian's first server.";
+
+    res.status(200).json({message: MOTD })
+})
+
 server.use('/api', apiRouter);
 
 module.exports = server;
